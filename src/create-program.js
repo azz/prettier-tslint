e06519376f9acb30d6e05e02be49a4505d0b7dd2
aleclarson <alec.stanford.larson@gmail.com>
2018-12-10 11:51:12 -0500

    fix: ensure the `parent` property exists on TS nodes

--- src/create-program.js
+++ src/create-program.js
@@ -6,6 +6,10 @@ const createProgram = filepath => {
     target: ts.ScriptTarget.Latest,
     jsx: "preserve",
   });
+
+  // This ensures the `parent` property of every node exists. More info here: https://github.com/Microsoft/TypeScript/issues/14464#issuecomment-284533993
+  program.getTypeChecker();
+
   return program;
 };
 
